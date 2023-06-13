425486ad303688f32c79fe81aa054020ca513fae
Jéssica Ramos <jessica.luiza297@gmail.com>
2023-06-13 13:24:00 -0300

    Métricas de avaliação + resultados de experimentos

--- sl-segmentation/compute_metrics.py
+++ sl-segmentation/compute_metrics.py
@@ -1,8 +1,11 @@
+import os
 import sys
 from args import FLAGS
 from dataset import get_datasets
 import numpy as np
 from glob import glob
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # define metrics
 def precision_score(y_true, y_pred):
@@ -32,33 +35,56 @@ def accuracy_score(y_true, y_pred):
     return sum(y_true==y_pred)/len(y_true)
 
 
+# define prediction based on probabilities
+def get_prediction(prob):
+    pred = []
+    for entry in prob:
+        if entry[0] > entry[1]:
+            pred.append(0)
+        else:
+            pred.append(1)
+    return pred
+
 FLAGS(sys.argv)
 
 # read datasets
 train, dev, test = get_datasets()
 
 # get labels into lists
-dev_labels = []
-for (input, label) in test:
-    dev_labels.append(label)
-
-print('Dev dataset:')
-print(f'Dev length: {len(dev_labels)}')
-print(f'Example label: {dev_labels[0]}')
-
 test_labels = []
 for (input, label) in test:
     test_labels.append(label)
 
-print('Test dataset:')
-print(f'Test length: {len(test_labels)}')
-print(f'Example label: {test_labels[0]}')
-
 # list all results saved in folder
-predictions = glob('./results/predictions/*.npy')
+pred_files = glob('./results/predictions/*.npy')
 
-print('.npy files:')
-for file in predictions:
-    print(f'   - {file}')
+# compute metrics for each model
+print('Test metrics:')
+for file in pred_files:
+    predictions = np.load(file, allow_pickle=True)
+    # compute metrics
+    acc = []
+    prec = []
+    rec = []
+    size = []
+    for i in range(predictions.shape[0]):
+        # compute metrics for each video
+        prob = np.array(predictions[i])
+        pred_labels = np.array(get_prediction(prob))
+        true_labels = test_labels[i].numpy()
+        true_labels = np.reshape(true_labels, -1)
+        acc.append(accuracy_score(true_labels, pred_labels))
+        prec.append(precision_score(true_labels, pred_labels))
+        rec.append(recall_score(true_labels, pred_labels))
+        size.append(pred_labels.shape[0])
+    # combine all videos using weighted average
+    acc_total = np.average(acc, weights=size)
+    prec_total = np.average(prec, weights=size)
+    rec_total = np.average(rec, weights=size)
 
-# for each model, get read probabilities, create prediction array (0 or 1), compute accuracy, precision and recall 
+    model = file.split('/')[-1]
+    print(f'\nModel: {model}')
+    print(f'    Accuracy: {acc_total}')
+    print(f'    Precision: {prec_total}')
+    print(f'    Recall: {rec_total}')
+    print(f'    F1 score: {(2*prec_total*rec_total)/(prec_total+rec_total)}')
